ab1e9c7d4ab69dea3fc511a04bd47149bbd74e4b
Himanshu Chauhan <himanshuchauhan@192.168.1.5>
2022-05-17 21:59:12 +0530

    Commenting if else program

--- 2_if-else.py
+++ 2_if-else.py
@@ -10,11 +10,15 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
+    # If n is odd, print Weird
     if n%2!=0:
         print("Weird")
+    # If n is even and in the inclusive range of 2 to 5, print Not Weird
     elif 2<=n<=5:
         print("Not Weird")
+    # If n is even and in the inclusive range of 6 to 20, print Weird
     elif 6<=n<=20:
         print("Weird")
+    # if n is greater than 20
     elif n>20:
         print("Not Weird")
