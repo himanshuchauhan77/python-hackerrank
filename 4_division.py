695ffe281ee238066818528f3d9a518426dadaaa
Himanshu Chauhan <himanshuchauhan@192.168.1.5>
2022-05-23 20:24:53 +0530

    arithmetic and division program

---
+++ 4_division.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+
+
+""" The / is floating point division, and the // is floor division, sometimes also called integer division."""
